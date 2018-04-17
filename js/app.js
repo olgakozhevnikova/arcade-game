2df0b443943814775a56ec169700a824092ce611
Olga Kozhevnikova <oolya.kozhevnikova@gmail.com>
2018-04-17 16:06:47 +0400

    Create the Player class

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function(x, y) {
+    // Variables applied to each of our instances go here
+    this.x = x;
+    this.y = y;
+    this.speed = 10;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = 'images/char-boy.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // You should multiply any movement by the dt parameter which will ensure the game runs at the same speed for all computers.
+    this.x += 10 * this.speed *dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,16 +22,51 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
+// Player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
+var Player = function() {
+    this.x = 100;
+    this.y = 400;
+    this.speed = 5;
+    this.sprite = 'images/char-boy.png';
+}
+
+Player.prototype.update = function(dt) {
+    this.x += 10 * this.speed *dt;
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {
+    if (37 in keyClick) {
+        player.x -= player.speed;
+    }
+
+    if (38 in keyClick) {
+        player.y -= player.speed;
+    }
+
+    if (39 in keyClick) {
+        player.x += player.speed;
+    }
+
+    if (40 in keyClick) {
+        player.y += player.speed;
+    }
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+const allEnemies = [new Enemy(-8, 60), new Enemy(0, 140), new Enemy(-5, 300)];
 
+const player = new Player(10, 140);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
